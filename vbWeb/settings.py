48ae68482a49184dde05e6e2d45614219017d801
Kushagra Sahu <kushagrasahu.ed@gmail.com>
2016-07-20 23:36:46 +0530

    Heroku error! Static files error

--- vbWeb/settings.py
+++ vbWeb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-*!8y^zddf^a7y=md-0nekmn@b%p$@r$%xr-r$4l+anl%u%w2v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
